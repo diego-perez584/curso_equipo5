69259828fd10664ff5b221528413cbf96be7c37f
Diego <diegoperez250603@gmail.com>
2025-10-20 17:41:53 -0600

    Version Aplicacion Angular Diego

--- app-angular/src/app/app.ts
+++ app-angular/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DiegoPerezComponent } from './diego-perez/diego-perez';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [DiegoPerezComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ app-angular/src/app/diego-perez/diego-perez.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-diego-perez',
+  templateUrl: './diego-perez.html',
+  styleUrls: ['./diego-perez.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class DiegoPerezComponent { }
